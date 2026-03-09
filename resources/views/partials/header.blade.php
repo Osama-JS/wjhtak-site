3eb8cb47f23b12ee7fd63bf133704c1975b88791
Osama-JS <osama.samomy@gmail.com>
2026-03-09 19:32:36 +0300

    update wjhtak

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -38,7 +38,7 @@
 									<path id="Path_1962" data-name="Path 1962" d="M26.373,26.526A6.667,6.667,0,0,0,20,21.833H6.667A6.667,6.667,0,0,0,.293,26.526,6.931,6.931,0,0,0,0,28.5V39.166a6.669,6.669,0,0,0,6.667,6.667H20a6.669,6.669,0,0,0,6.667-6.667V28.5A6.928,6.928,0,0,0,26.373,26.526ZM6.667,24.5H20a4.011,4.011,0,0,1,3.947,3.36L13.333,33.646,2.72,27.86A4.011,4.011,0,0,1,6.667,24.5ZM24,39.166a4.012,4.012,0,0,1-4,4H6.667a4.012,4.012,0,0,1-4-4V30.873L12.693,36.34a1.357,1.357,0,0,0,1.28,0L24,30.873Z" transform="translate(0 0)" fill="#135846"/>
 								  </g>
 								</svg>
-									<span class="badge light text-white bg-primary rounded-circle">0</span>
+									<!-- <span class="badge light text-white bg-primary rounded-circle">0</span> -->
 								</a>
 							</li>
 							<li class="nav-item dropdown notification_dropdown">
@@ -49,7 +49,7 @@
 									  </g>
 									</svg>
 
-									<span class="badge light text-white bg-primary rounded-circle">0</span>
+									<!-- <span class="badge light text-white bg-primary rounded-circle">0</span> -->
 								</a>
 								<div class="dropdown-menu dropdown-menu-end">
 									<div id="DZ_W_Notification1" class="widget-media dlab-scroll p-3" style="height:380px;">
@@ -78,7 +78,7 @@
 										<path id="Path_1956" data-name="Path 1956" d="M274.4,67.347a4.8,4.8,0,0,0-4.8-4.8H255.2a4.8,4.8,0,0,0-4.8,4.8v15.6a1.2,1.2,0,0,0,2.048.848l3.746-3.745a2.4,2.4,0,0,1,1.7-.7H269.6a4.8,4.8,0,0,0,4.8-4.8Zm-2.4,0a2.4,2.4,0,0,0-2.4-2.4H255.2a2.4,2.4,0,0,0-2.4,2.4v12.7l1.7-1.7a4.8,4.8,0,0,1,3.395-1.406H269.6a2.4,2.4,0,0,0,2.4-2.4Zm-15.6,7.2H266a1.2,1.2,0,1,0,0-2.4h-9.6a1.2,1.2,0,0,0,0,2.4Zm0-4.8h12a1.2,1.2,0,1,0,0-2.4h-12a1.2,1.2,0,0,0,0,2.4Z" fill="#135846" fill-rule="evenodd"/>
 									  </g>
 									</svg>
-									<span class="badge light text-white bg-primary rounded-circle">0</span>
+									<!-- <span class="badge light text-white bg-primary rounded-circle">0</span> -->
 								</a>
 								<div class="dropdown-menu dropdown-menu-end">
 									<div id="DZ_W_TimeLine02" class="widget-timeline dlab-scroll style-1 ps ps--active-y p-3 height370">
